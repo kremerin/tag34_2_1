af9457f2d8c5daad2a14b1a5aeeefc34604590ab
Kremerin <110621622+kremerin@users.noreply.github.com>
2022-08-25 18:14:40 +0200

    Vers1

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let headline = document.getElementById("headline");
+
+function change() {
+    headline.classList.add("changedHeadline");
+}
+
+function changeReturn() {
+    headline.classList.remove("changedHeadline")
+}
